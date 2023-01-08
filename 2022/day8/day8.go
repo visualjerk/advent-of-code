3578d2628f0250ceb87a7493940f02581d6288ab
Jörg Bayreuther <jb@visualjerk.de>
2023-01-08 01:35:34 +0100

    solve day8

--- 2022/day8/day8.go
+++ 2022/day8/day8.go
@@ -6,39 +6,106 @@ import (
 )
 
 type Tree struct {
+	row    int
+	col    int
 	height int
-	hidden bool
 }
 
-type TreeGrid [][]Tree
+type TreeGroup []Tree
 
-func (grid *TreeGrid) addTree(rowIndex, height int) {
-	if len(*grid) < rowIndex+1 {
-		*grid = append(*grid, []Tree{})
+func (treeGroup *TreeGroup) addTree(tree Tree) {
+	*treeGroup = append(*treeGroup, tree)
+}
+
+func (treeGroup *TreeGroup) getCol(col int) TreeGroup {
+	result := TreeGroup{}
+	for _, tree := range *treeGroup {
+		if tree.col == col {
+			result.addTree(tree)
+		}
 	}
-	(*grid)[rowIndex] = append((*grid)[rowIndex], Tree{
-		height: height,
-	})
+	return result
 }
 
-func parseData(data string) TreeGrid {
-	grid := TreeGrid{}
+func (treeGroup *TreeGroup) getRow(row int) TreeGroup {
+	result := TreeGroup{}
+	for _, tree := range *treeGroup {
+		if tree.row == row {
+			result.addTree(tree)
+		}
+	}
+	return result
+}
+
+func (treeGroup *TreeGroup) getSurroundingGroups(tree Tree) []TreeGroup {
+	col := treeGroup.getCol(tree.col)
+	top := col[0:tree.row]
+	bottom := col[tree.row+1:]
+
+	row := treeGroup.getRow(tree.row)
+	left := row[0:tree.col]
+	right := row[tree.col+1:]
+
+	return []TreeGroup{
+		top,
+		bottom,
+		left,
+		right,
+	}
+}
+
+func (treeGroup *TreeGroup) isLowerThan(tree Tree) bool {
+	for _, coverTree := range *treeGroup {
+		if tree.height <= coverTree.height {
+			return false
+		}
+	}
+	return true
+}
+
+func (treeGroup *TreeGroup) isTreeVisible(tree Tree) bool {
+	surroundingGroups := treeGroup.getSurroundingGroups(tree)
+	for _, group := range surroundingGroups {
+		if group.isLowerThan(tree) {
+			return true
+		}
+	}
+	return false
+}
+
+func (treeGroup *TreeGroup) countVisibleTrees() int {
+	count := 0
+	for _, tree := range *treeGroup {
+		if treeGroup.isTreeVisible(tree) {
+			count++
+		}
+	}
+	return count
+}
+
+func parseData(data string) TreeGroup {
+	treeGroup := TreeGroup{}
 	rows := strings.Split(data, "\n")
 
 	for rowIndex, row := range rows {
 		trees := strings.Split(row, "")
-		for _, rawHeight := range trees {
+		for colIndex, rawHeight := range trees {
 			height := utils.SafeStringToInt(rawHeight)
-			grid.addTree(rowIndex, height)
+			treeGroup.addTree(Tree{
+				row:    rowIndex,
+				col:    colIndex,
+				height: height,
+			})
 		}
 	}
 
-	return grid
+	return treeGroup
 }
 
 func main() {
 	data := utils.LoadData()
 	treeGrid := parseData(data)
+	visibleTreeCount := treeGrid.countVisibleTrees()
 
-	println(treeGrid)
+	println(visibleTreeCount)
 }
