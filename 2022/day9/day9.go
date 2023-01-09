a69bb0c0e678a616a05da67cff4880bed68bd371
Jörg Bayreuther <jb@visualjerk.de>
2023-01-09 22:53:01 +0100

    visualize day9

--- 2022/day9/day9.go
+++ 2022/day9/day9.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"strings"
+	"time"
 
 	"aoc.io/utils"
 )
+
+const GRID_LENGTH = 50
+const FRAME_TIME = time.Millisecond * 100
 
 type Direction int
 
@@ -22,9 +26,59 @@ var RAW_DIRECTION_MAP = map[string]Direction{
 	"R": RIGHT,
 }
 
+var MOVE_INDICATOR = map[Direction]string{
+	UP:    "⬆️",
+	DOWN:  "⬇️",
+	LEFT:  "⬅️",
+	RIGHT: "➡️",
+}
+
 type Move struct {
 	direction Direction
 	distance  int
+}
+
+type Head struct {
+	x int
+	y int
+}
+
+func (head *Head) move(direction Direction) {
+	switch direction {
+	case UP:
+		head.y++
+	case DOWN:
+		head.y--
+	case RIGHT:
+		head.x++
+	case LEFT:
+		head.x--
+	}
+}
+
+func (head *Head) renderState() {
+	for y := 0; y < GRID_LENGTH; y++ {
+		for x := 0; x < GRID_LENGTH; x++ {
+			if x == head.x && y == head.y {
+				print("H")
+			} else {
+				print(".")
+			}
+		}
+		print("\n")
+	}
+}
+
+func executeMoves(moves []Move) {
+	head := Head{GRID_LENGTH / 2, GRID_LENGTH / 2}
+	for moveIndex, move := range moves {
+		for i := 0; i < move.distance; i++ {
+			head.move(move.direction)
+			head.renderState()
+			println("move ", moveIndex+1, "/", len(moves), " | dir ", MOVE_INDICATOR[move.direction], " | distance ", move.distance)
+			time.Sleep(FRAME_TIME)
+		}
+	}
 }
 
 func parseRawMove(data string) Move {
@@ -49,6 +103,7 @@ func parseData(data string) (moves []Move) {
 func main() {
 	data := utils.LoadData()
 	moves := parseData(data)
+	executeMoves(moves)
 
 	println(moves)
 }
